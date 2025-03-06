3aa1d2a221b7cda516f73c08f8eb832b62b0fe6b
muryllovieira <muryllovieira59@gmail.com>
2025-03-06 15:55:38 -0300

    testando components do react

--- App.tsx
+++ App.tsx
@@ -1,34 +1,72 @@
 import { StatusBar } from 'expo-status-bar';
-import { Alert, StyleSheet, Text, View } from 'react-native';
+import { Alert, StyleSheet, Text, View, Image, TextInput, Button, ScrollView, Switch } from 'react-native';
+import React, { useState } from 'react';
+import imgSuperman from './assets/superman.png'
 
 export default function App() {
+
+  const [usuario, setUsuario] = useState('A')
+  const [ligado, setLigado] = useState(false)
+
+  function handleSwitch(){
+    setLigado(!ligado)
+  }
+
   return (
-    <View style={styles.container}>
+    <ScrollView>
+      <View style={styles.container}>
 
-      <View
-        onTouchStart={(event) => {
-          Alert.alert('Começou a clicar', 'Clique iniciado')
-        }}
-        onTouchEnd={(evento) => {
-          Alert.alert('Acabou de clicar', 'Clique finalizado')
-        }}
-      >
-        <Text style={[styles.texto, styles.border]}>Olá</Text>
+        <Switch 
+          value={ligado}
+          onValueChange={handleSwitch}
+          style={{ paddingTop: 50 }}
+        >
+        </Switch>
+
+        <Image
+          source={imgSuperman}
+          style={{display: ligado ? 'flex' : 'none'}}
+        />
+
+        <TextInput
+          style={styles.input}
+          onChange={(text) => setUsuario(text.nativeEvent.text)
+          }
+          keyboardType='default'
+          placeholder='digite seu numero'
+          value={usuario}
+        />
+        <Button
+          title='click aqui'
+          onPress={() => Alert.alert('valor atual', usuario)}
+        />
+
+        <View
+          onTouchStart={(event) => {
+            Alert.alert('Começou a clicar', 'Clique iniciado')
+          }}
+          onTouchEnd={(evento) => {
+            Alert.alert('Acabou de clicar', 'Clique finalizado')
+          }}
+        >
+          <Text style={[styles.texto, styles.border]}>Olá</Text>
+        </View>
+
+        <Text selectable={true}
+          onPress={() => {
+            console.log("PRESSIONADO");
+          }}
+          onLongPress={() => { console.log("pressionamento longo") }}
+        >Open up App.tsx to start working on your app!</Text>
+        <StatusBar style="auto" />
+
+        <View>
+          <Text>ola</Text>
+          <Text>mundo</Text>
+        </View>
+
       </View>
-
-      <Text selectable={true}
-        onPress={() => {console.log("PRESSIONADO");
-        }}
-        onLongPress={() => {console.log("pressionamento longo")}}
-      >Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-
-      <View>
-        <Text>ola</Text>
-        <Text>mundo</Text>
-      </View>
-
-    </View>
+    </ScrollView>
   );
 }
 
@@ -45,6 +83,13 @@ const styles = StyleSheet.create({
     padding: 10,
     borderRadius: 10,
     marginBottom: 10,
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    backgroundColor: '#fff',
   },
   texto: {
     fontSize: 20,
